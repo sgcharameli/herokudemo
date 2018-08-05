ef995ffb9632654f239abfb1692ade53f787b327
Sergio García Charameli <sgarciach@sanitas.es>
2018-08-05 11:15:20 +0200

    Se sube recurso monitor

---
+++ src/main/java/es/sergio/herokudemo/controladores/Monitor.java
@@ -0,0 +1,27 @@
+package es.sergio.herokudemo.controladores;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import es.sergio.herokudemo.controladores.vo.MonitorVO;
+
+@Controller
+@RequestMapping("Monitor")
+public class Monitor {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Monitor.class);
+
+	public Monitor() {
+		LOGGER.info("Servicio '/Monitor' arrancado correctamente");
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public MonitorVO estado() {
+		return new MonitorVO();
+	}
+}
---
+++ src/main/java/es/sergio/herokudemo/controladores/vo/MonitorVO.java
@@ -0,0 +1,19 @@
+package es.sergio.herokudemo.controladores.vo;
+
+public class MonitorVO {
+
+	private String estado;
+	
+	public MonitorVO() {
+		this.estado = "ok";
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+}
